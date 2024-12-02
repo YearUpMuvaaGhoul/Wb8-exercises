04ecb045f2d8cfa1eaed746304a9bf96c163db90
Lexx Matula <alexamatula@gmail.com>
2024-12-02 11:22:59 -0500

    created northwind products project

--- NorthwindProducts/src/main/java/com/pluralsight/Main.java
+++ NorthwindProducts/src/main/java/com/pluralsight/Main.java
@@ -11,20 +11,20 @@ public class Main {
 // use the database URL to point to the correct database
         Connection connection;
         connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/sakila",
-                 "newuser",
-                 "yearup2424");
+                "jdbc:mysql://localhost:3306/northwind",
+                "newuser",
+                "yearup2424");
 
 // create statement
 // the statement is tied to the open connection
         Statement statement = connection.createStatement();
 // define your query
-        String query = "SELECT * FROM sakila.city WHERE Country_ID = 103";
+        String query = "SELECT * FROM northwind.products";
 // 2. Execute your query
         ResultSet results = statement.executeQuery(query);
 // process the results
         while (results.next()) {
-            String city = results.getString("city");
+            String city = results.getString("productname");
             System.out.println(city);
         }
 // 3. Close the connection
